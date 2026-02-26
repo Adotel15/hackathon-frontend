bec94212722b01bf269c3dcf641f8b00585860e9
Adrian Dotel <adrian.dotel.sge@mango.com>
2026-02-26 22:11:32 +0100

    t

--- app/api/order-confirmation/route.ts
+++ app/api/order-confirmation/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 const EXTERNAL_URL =
-  "http://aiternative-mango-alb-1984945680.us-east-1.elb.amazonaws.com/order-confirmation?productId=27025826-43&mail=adriandotel@gmail.com";
+  "http://aiternative-mango-alb-1984945680.us-east-1.elb.amazonaws.com/order-confirmation?productId=27025826-43&mail=joan.busquet@mango.com";
 
 export async function GET() {
   try {
