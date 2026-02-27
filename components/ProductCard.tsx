9fe8e84233fb3b5e03e1c236a06307b68254f2d0
Adrian Dotel <adrian.dotel.sge@mango.com>
2026-02-27 08:04:57 +0100

    t

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,20 +5,20 @@ export default function ProductCard() {
     <div className="flex gap-4">
       <div className="w-[90px] h-[130px] relative flex-shrink-0">
         <Image
-          src="https://shop.mango.com/assets/rcs/pics/static/T1/fotos/outfit/S/17071186_70-99999999_01.jpg?imwidth=2048&imdensity=1"
-          alt="Vestido punto fino evasé"
+          src="https://shop.mango.com/assets/rcs/pics/static/T8/fotos/outfit/S/87017198_70-99999999_01.jpg?imwidth=2048&imdensity=1"
+          alt="Vestido Lyocell cuello drapeado"
           fill
           className="object-cover"
           sizes="90px"
         />
       </div>
       <div className="flex flex-col justify-center text-sm">
-        <p className="text-xs text-gray-500 mb-1">REF. 17071186-70</p>
-        <p className="font-normal mb-1">Vestido punto fino evasé</p>
-        <p className="mb-1">39,99 €</p>
+        <p className="text-xs text-gray-500 mb-1">REF. 87017198-70</p>
+        <p className="font-normal mb-1">Vestido Lyocell cuello drapeado</p>
+        <p className="mb-1">49,99 €</p>
         <div className="flex gap-4 text-sm">
           <p>Talla: M</p>
-          <p>Color: Rojo</p>
+          <p>Color: 70</p>
         </div>
       </div>
     </div>
